e42491ee7afbf0b9befb6e76d9c102a137cf520c
Logan Bell <loganbell@gmail.com>
2015-04-28 21:35:36 -0700

    Moar

--- thread_safe_slice_via_channels/main.go
+++ thread_safe_slice_via_channels/main.go
@@ -9,9 +9,9 @@ type commandAction int
 const (
 	append1 commandAction = iota
 	at
-	close
+	close1
 	delete
-	len
+	len1
 	update
 )
 
@@ -22,18 +22,20 @@ type commandData struct {
 	index int
 	value interface{}
 	result chan<-interface{}
+	data chan<-[]interface{}
+	cb UpdateFunc
 }
 
 type SafeSlice interface {
 	Append(interface{})
 	At(int) interface{}
-//	Close() []interface{}
+	Close() []interface{}
 	Delete (int)
-//	Len () int
-//	Update(int, UpdateFunc)
+	Len () int
+	Update(int, UpdateFunc)
 }
 
-type UpdateFunc func(interface{}, bool) interface {}
+type UpdateFunc func(interface{}) interface {}
 
 func (ss safeSlice) Append(value interface{}) {
 	ss<-commandData{action: append1, value: value}
@@ -43,9 +45,27 @@ func (ss safeSlice) Delete(index int) {
 	ss<-commandData{action: delete, index: index}
 }
 
+func (ss safeSlice) Update(index int, cb UpdateFunc) {
+	ss<-commandData{action: update, index: index, cb: cb}
+}
+
+func (ss safeSlice) Len() int {
+	reply := make(chan interface{})
+	ss<-commandData{action: len1, result: reply}
+	result := <-reply
+	return result.(int)
+}
+
 func (ss safeSlice) At(index int) interface{} {
 	reply := make(chan interface{})
 	ss <- commandData{action: at, index: index, result: reply }
+	result := <-reply
+	return result
+}
+
+func (ss safeSlice) Close() []interface{} {
+	reply := make(chan []interface{})
+	ss <- commandData{action: close1, data: reply }
 	result := <-reply
 	return result
 }
@@ -63,6 +83,16 @@ func (ss safeSlice) Run () {
 		case at:
 			value := store[command.index]
 			command.result <- value
+		case len1:
+			value := len(store)
+			command.result <- value
+		case update:
+			i := command.index
+			cb := command.cb
+			store[i] = cb(store[i])
+		case close1:
+			close(ss)
+			command.data <- store
 		}
 	}
 }
@@ -79,7 +109,16 @@ func main() {
 	fmt.Println(test.At(0))
 	test.Append("bar")
 	fmt.Println(test.At(1))
+	fmt.Println(test.Len())
 	test.Delete(0)
 	fmt.Println(test.At(0))
+	fmt.Println(test.Len())
 
+	cb := func(v interface{}) interface{} {
+		return "woop!"
+	}
+	test.Update(0, cb)
+	fmt.Println(test.At(0))
+
+	fmt.Println(test.Close())
 }
